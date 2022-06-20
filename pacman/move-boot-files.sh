0605fa048e4d75a098505b16a56ba05f9ba96b69
Hetsh <git@hetsh.de>
2022-06-20 17:46:07 +0200

    Add "move-boot-files" scripts in debug mode

---
+++ pacman/move-boot-files.sh
@@ -0,0 +1,26 @@
+#/usr/bin/env bash
+
+set -e -u
+echo "Running in debug-mode!"
+
+BOOT_DIR="/boot"
+BOOT_MOUNTS=$(cat /proc/mounts | grep "$BOOT_DIR" | cut -d ' ' -f 2)
+distribute() {
+	SOURCE="$1"
+	if [ -f "$SOURCE" ]; then
+		echo "Found $SOURCE. Distributing it to..."
+		for MOUNT in $BOOT_MOUNTS; do
+			echo -e "\t$MOUNT"
+			echo cp -a "$SOURCE" "$MOUNT"
+		done
+		echo "... done! Deleting $SOURCE"
+		echo rm "$SOURCE"
+	fi
+}
+
+KERNEL=$(pacman -Qq | grep zfs-linux)
+distribute "$BOOT_DIR/vmlinuz-$KERNEL"
+distribute "$BOOT_DIR/initramfs-$KERNEL.img"
+
+MICROCODE=$(pacman -Qq | grep ucode)
+distribute "$BOOT_DIR/$MICROCODE.img"
